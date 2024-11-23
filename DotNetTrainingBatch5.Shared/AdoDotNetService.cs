d53f12df2f4f53920c2e01fcfb561a35515de5da
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-11-23 23:05:40 +0630

    fix: code changes

--- DotNetTrainingBatch5.Shared/AdoDotNetService.cs
+++ DotNetTrainingBatch5.Shared/AdoDotNetService.cs
@@ -1,71 +1,70 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 
-namespace DotNetTrainingBatch5.Shared
+namespace DotNetTrainingBatch5.Shared;
+
+public class AdoDotNetService
 {
-    public class AdoDotNetService
+    private readonly string _connectionString;
+
+    public AdoDotNetService(string connectionString)
     {
-        private readonly string _connectionString;
-
-        public AdoDotNetService(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
-
-        public DataTable Query(string query, params SqlParameterModel[] sqlParameters)
-        {
-            SqlConnection connection = new SqlConnection(_connectionString);
-            connection.Open();
-
-            SqlCommand cmd = new SqlCommand(query, connection);
-            if (sqlParameters is not null)
-            {
-                foreach (var sqlParameter in sqlParameters)
-                {
-                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
-                }
-            }
-
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
-
-            connection.Close();
-
-            return dt;
-        }
-
-        public int Execute(string query, params SqlParameterModel[] sqlParameters)
-        {
-            SqlConnection connection = new SqlConnection(_connectionString);
-            connection.Open();
-
-            SqlCommand cmd = new SqlCommand(query, connection);
-            if (sqlParameters is not null)
-            {
-                foreach (var sqlParameter in sqlParameters)
-                {
-                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
-                }
-            }
-            var result = cmd.ExecuteNonQuery();
-
-            connection.Close();
-
-            return result;
-        }
+        _connectionString = connectionString;
     }
 
-    public class SqlParameterModel
+    public DataTable Query(string query, params SqlParameterModel[] sqlParameters)
     {
-        public string Name { get; set; }
-        public object Value { get; set; }
+        SqlConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
 
-        public SqlParameterModel() { }
-        public SqlParameterModel(string name, object value)
+        SqlCommand cmd = new SqlCommand(query, connection);
+        if (sqlParameters is not null)
         {
-            Name = name;
-            Value = value;  
+            foreach (var sqlParameter in sqlParameters)
+            {
+                cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+            }
         }
+
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adapter.Fill(dt);
+
+        connection.Close();
+
+        return dt;
+    }
+
+    public int Execute(string query, params SqlParameterModel[] sqlParameters)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(query, connection);
+        if (sqlParameters is not null)
+        {
+            foreach (var sqlParameter in sqlParameters)
+            {
+                cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+            }
+        }
+        var result = cmd.ExecuteNonQuery();
+
+        connection.Close();
+
+        return result;
+    }
+}
+
+public class SqlParameterModel
+{
+    public string Name { get; set; }
+    public object Value { get; set; }
+
+    public SqlParameterModel() { }
+    public SqlParameterModel(string name, object value)
+    {
+        Name = name;
+        Value = value;  
     }
 }
--- DotNetTrainingBatch5.Shared/DapperService.cs
+++ DotNetTrainingBatch5.Shared/DapperService.cs
@@ -7,37 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNetTrainingBatch5.Shared
+namespace DotNetTrainingBatch5.Shared;
+
+public class DapperService
 {
-    public class DapperService
+    private readonly string _connectionString;
+
+    public DapperService(string connectionString)
     {
-        private readonly string _connectionString;
+        _connectionString = connectionString;
+    }
 
-        public DapperService(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+    public List<T> Query<T>(string query, object? param = null)
+    {
+        //Console.WriteLine("Query is: " + query);
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var lst = db.Query<T>(query, param).ToList();
+        return lst;
+    }
 
-        public List<T> Query<T>(string query, object? param = null)
-        {
-            //Console.WriteLine("Query is: " + query);
-            using IDbConnection db = new SqlConnection(_connectionString);
-            var lst = db.Query<T>(query, param).ToList();
-            return lst;
-        }
+    public T QueryFirstOrDefault<T>(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var item = db.QueryFirstOrDefault<T>(query, param);
+        return item;
+    }
 
-        public T QueryFirstOrDefault<T>(string query, object? param = null)
-        {
-            using IDbConnection db = new SqlConnection(_connectionString);
-            var item = db.QueryFirstOrDefault<T>(query, param);
-            return item;
-        }
-
-        public int Execute(string query, object? param = null)
-        {
-            using IDbConnection db = new SqlConnection(_connectionString);
-            var result = db.Execute(query, param);
-            return result;
-        }
+    public int Execute(string query, object? param = null)
+    {
+        using IDbConnection db = new SqlConnection(_connectionString);
+        var result = db.Execute(query, param);
+        return result;
     }
 }
