3455144d6e634783500d2176154004d9dbd896db
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-11-25 00:59:23 +0630

    feat: POS DataBase

---
+++ DotNetTraningBatch5.PointOfSale.DataBase/Models/TblCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetTrainingBatch5.PointOfSale.DataBase.Models;
+
+public partial class TblCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryCode { get; set; } = null!;
+
+    public string CategoryName { get; set; } = null!;
+
+    public bool DeleteFlag { get; set; }
+
+    public virtual ICollection<TblProduct> TblProducts { get; set; } = new List<TblProduct>();
+}
---
+++ DotNetTraningBatch5.PointOfSale.DataBase/Models/TblSaleDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetTrainingBatch5.PointOfSale.DataBase.Models;
+
+public partial class TblSaleDetail
+{
+    public int DetailId { get; set; }
+
+    public string? DetailCode { get; set; }
+
+    public string ProductCode { get; set; } = null!;
+
+    public int ProductQuantity { get; set; }
+
+    public decimal Total { get; set; }
+
+    public string SaleCode { get; set; } = null!;
+
+    public bool DeleteFlag { get; set; }
+
+    public virtual TblProduct ProductCodeNavigation { get; set; } = null!;
+
+    public virtual TblSale SaleCodeNavigation { get; set; } = null!;
+}
