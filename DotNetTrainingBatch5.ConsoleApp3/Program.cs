3844efb909af9a900a355ff9628448f9ff934881
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-14 22:40:05 +0630

    feat: GUID & ULID

--- DotNetTrainingBatch5.ConsoleApp3/Program.cs
+++ DotNetTrainingBatch5.ConsoleApp3/Program.cs
@@ -20,11 +20,25 @@ Console.WriteLine("Hello, World!");
 //await httpClientExample.Create("test title", "test body", 1);
 //await httpClientExample.Update(1, "test title", "test body", 10);
 
-Console.Write("waiting for api...");
-Console.ReadLine();
+//Console.Write("waiting for api...");
+//Console.ReadLine();
 
-RefitExample refitExample = new RefitExample();
-await refitExample.Run();
+//RefitExample refitExample = new RefitExample();
+//await refitExample.Run();
 
+var id = Guid.NewGuid();
+var id2 = Ulid.NewUlid();
+Console.WriteLine(id.ToString());
+Console.WriteLine(id2.ToString());
+Console.WriteLine(id2.ToString().Length);
+
+Console.WriteLine(id2.ToString().ToLower().Substring(0, 3) + "-" + id2.ToString().ToLower().Substring(4, 3));
+
+
+Random r = new Random();
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(r.Next(1, 7));
+}
 
 Console.ReadLine();
